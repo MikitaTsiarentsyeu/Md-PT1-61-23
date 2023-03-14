7ceecf8bd3961ef0397fc6f0afd003ff5c7bfd3d
Anna7777777777 <alinovskaya_a@mail.ru>
2023-03-14 14:50:02 +0300

    finish Task1

--- Tasks/Alinovskaya/Task1.py
+++ Tasks/Alinovskaya/Task1.py
@@ -1,1 +1,31 @@
-print('hi');
+
+import math;
+import decimal;
+import random;
+
+# 1 
+degrees_celcia = 4;
+degrees_fahrenheit = degrees_celcia * 1,8 + 32;
+print('converts Celsius to Fahrenheit', degrees_fahrenheit);
+
+#2
+num_radius = 52;
+area_of_circle = round(math.pi * num_radius**2);
+print('area of circle equal', area_of_circle);
+
+#3
+km_pr_hr = 120;
+mt_pr_sc = round(km_pr_hr * (1000/3600))
+print(mt_pr_sc, 'meters per second');
+
+#4
+sum_money_USA = 100;
+exchange_rates = 2.85;
+sum_money_BLR = decimal.Decimal(sum_money_USA * exchange_rates);
+print('money exchange - ', sum_money_BLR, 'blr');
+
+#5
+a = -888;
+b = 888;
+random_number = random.randint(a, b);
+print(random_number, 'it is a random number between in a given range');
