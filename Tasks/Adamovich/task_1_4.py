9a1c1975132a35e0c00f2a525c31491f980922fc
Anastasiya_Adamovich <adamovichanastasiyas@gmail.com>
2023-03-19 15:21:29 +0300

    add_task1 with some corrections

--- Tasks/Adamovich/task_1_4.py
+++ Tasks/Adamovich/task_1_4.py
@@ -1,6 +1,7 @@
-#Write a program that converts some amount of money from USD to BYN, the amount and ratio are given.
+#4 Write a program that converts some amount of money from USD to BYN, the amount and ratio are given.
 import decimal
 dollars_per_byn = decimal.Decimal("2.4")
 dollars_count = decimal.Decimal(input("Input amount in dollars: "))
 byn_count = dollars_count * dollars_per_byn
+print(f" {dollars_count} $ is {byn_count} BYN ")
 print(byn_count)
--- Tasks/Adamovich/task_1_5.py
+++ Tasks/Adamovich/task_1_5.py
@@ -2,4 +2,4 @@
 import random
 range_low = int(input("Input the lower limit of the interval as integer: "))
 range_upper = int(input("Input the upper limit of the interval as integer: "))
-print(random.randint(range_low, range_upper))
+print(f" Random number btween {range_low} and {range_upper} is {random.randint(range_low, range_upper)}")
