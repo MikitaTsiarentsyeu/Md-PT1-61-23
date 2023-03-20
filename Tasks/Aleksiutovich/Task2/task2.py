f4056ed7cd1c8c67fe26f402537d69543560d022
OxY623 <fuzzydoorproductions0@gmail.com>
2023-03-20 23:13:10 +0300

    Update and reduction  task2.py

--- Tasks/Aleksiutovich/Task2/task2.py
+++ Tasks/Aleksiutovich/Task2/task2.py
@@ -3,16 +3,9 @@ import datetime as dt
 
 '''without checking for the correctness of the input'''
 
-
 # Implement a text output of the time entered from the console (the user should input data in the format hh:mm).
 #
 # Show the responses to the user in Russian according to the rules listed below:
-#
-# min == 0: такое-то значение часа ровно (15:00 - три часа ровно)
-# min < 30: столько-то минут следующего часа (19:12 - двенадцать минут восьмого)
-# min == 30: половина такого-то (15:30 - половина четвёртого)
-# min > 30 and min < 45 столько-то минут следующего часа (12:38 - тридцать восемь минут первого)
-# min >= 45 без min такого-то (08:54 - без шести минут девять)
 class СonvertingTime:
     def __init__(self):
         self.dictionary_1 = {1: "Один", 2: "Два", 3: "Три", 4: "Четыре", 5: "Пять", 6: "Шесть", 7: "Семь",
@@ -35,68 +28,76 @@ class СonvertingTime:
     def input_time_find_hour_and_minute(self):
         self.digital_time_input = str(input("Введите время в формате hh:mm.\n Пример, 16:00 \n \t")).strip()
         # self.digital_time_input = "12:58"
-        # print(self.digital_time_input)
         self.data_time = dt.datetime.strptime(self.digital_time_input, "%H:%M")
         self.hour = int(self.data_time.hour)
         self.minute = int(self.data_time.minute)
-        # print(self.minute)
-        # print(self.hour)
+    def modul_1(self):
+        if self.hour > 12:
+            if self.minute in self.list_2:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
+                      f"{self.dictionary_3[self.minute % 10]} "
+                      f"минута {self.dictionary_2[self.hour + 1 - 12]}")
+            elif self.minute in self.list:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
+                      f"{self.dictionary_3[self.minute % 10]}  "
+                      f"минуты {self.dictionary_2[self.hour + 1 - 12]}")
+            else:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute]} "
+                      f"минут {self.dictionary_2[self.hour + 1 - 12]}")
+        else:
+            if self.minute in self.list_2:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
+                      f"{self.dictionary_3[self.minute % 10]}  "
+                      f"минута {self.dictionary_2[self.hour + 1]}")
+            elif self.minute in self.list:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
+                      f"{self.dictionary_3[self.minute % 10]}  "
+                      f"минуты {self.dictionary_2[self.hour + 1]}")
+            else:
+                print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
+                      f"{self.dictionary_3[self.minute % 10]} "
+                      f"минут {self.dictionary_2[self.hour + 1]}")
+    def modul_2(self):
+        if self.hour == 1 or self.hour == 13:
+            if self.hour > 12:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour - 12].lower()} час ровно")
+            else:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour].lower()} час ровно")
+        elif self.hour in self.list_3:
+            if self.hour > 12:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour - 12].lower()} часа ровно")
+            else:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour].lower()} часа ровно")
+        else:
+            if self.hour > 13:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour - 12].lower()} часов ровно")
+            else:
+                print(f"{self.digital_time_input} - "
+                      f"{self.dictionary_1[self.hour].lower()} часов ровно")
+
+    def modul_3(self):
+        if self.hour > 12:
+            print(
+                f"{self.digital_time_input} - без {self.dictionary_4[60 - self.minute]} "
+                f"минут {self.dictionary_2[self.hour + 1 - 12]}")
+        else:
+            print(
+                f"{self.digital_time_input} - без {self.dictionary_4[60 - self.minute]} "
+                f"минут {self.dictionary_2[self.hour + 1]}")
 
     def logic_of_actions(self):
         self.input_time_find_hour_and_minute()
-        # print(self.minute)
-        # print(self.hour)
-        # print(self.dictionary_1)
         # min == 0: такое-то значение часа ровно (15:00 - три часа ровно)
         if self.minute == 0:
-            if self.hour == 1 or self.hour == 13:
-                if self.hour > 12:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour - 12].lower()} час ровно")
-                else:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour].lower()} час ровно")
-            elif self.hour in  self.list_3:
-                if self.hour > 12:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour - 12].lower()} часа ровно")
-                else:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour].lower()} часа ровно")
-            else:
-                if self.hour > 13:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour - 12].lower()} часов ровно")
-                else:
-                    print(f"{self.digital_time_input} - "
-                          f"{self.dictionary_1[self.hour].lower()} часов ровно")
+            self.modul_2()
         # min < 30: столько-то минут следующего часа (19:12 - двенадцать минут восьмого)
         elif self.minute < 30:
-            if self.hour > 12:
-                if self.minute in self.list_2:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]} "
-                          f"минута {self.dictionary_2[self.hour + 1 - 12]}")
-                elif self.minute in self.list:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минуты {self.dictionary_2[self.hour + 1 - 12]}")
-                else:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute]} "
-                          f"минут {self.dictionary_2[self.hour + 1 - 12]}")
-            else:
-                if self.minute in self.list_2:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минута {self.dictionary_2[self.hour + 1]}")
-                elif self.minute in self.list:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минуты {self.dictionary_2[self.hour + 1]}")
-                else:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]} "
-                          f"минут {self.dictionary_2[self.hour + 1]}")
+            self.modul_1()
         # min == 30: половина такого-то (15:30 - половина четвёртого)
         elif self.minute == 30:
             if self.hour > 12:
@@ -105,41 +106,10 @@ class СonvertingTime:
                 print(f"{self.digital_time_input} - половина {self.dictionary_2[self.hour + 1]}")
 
         elif self.minute > 30 and self.minute < 45:
-            if self.hour > 12:
-                if self.minute in self.list_2:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]} "
-                          f"минута {self.dictionary_2[self.hour + 1 - 12]}")
-                elif self.minute in self.list:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минуты {self.dictionary_2[self.hour + 1 - 12]}")
-                else:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute]} "
-                          f"минут {self.dictionary_2[self.hour + 1 - 12]}")
-            else:
-                if self.minute in self.list_2:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минута {self.dictionary_2[self.hour + 1]}")
-                elif self.minute in self.list:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]}  "
-                          f"минуты {self.dictionary_2[self.hour + 1]}")
-                else:
-                    print(f"{self.digital_time_input} - {self.dictionary_3[self.minute - self.minute % 10]} "
-                          f"{self.dictionary_3[self.minute % 10]} "
-                          f"минут {self.dictionary_2[self.hour + 1]}")
+            self.modul_1()
         # min >= 45 без min такого-то (08:54 - без шести минут девять)
         elif self.minute >= 45:
-            if self.hour > 12:
-                print(
-                    f"{self.digital_time_input} - без {self.dictionary_4[60 - self.minute]} "
-                    f"минут {self.dictionary_2[self.hour + 1 - 12]}")
-            else:
-                print(
-                    f"{self.digital_time_input} - без {self.dictionary_4[60 - self.minute]} "
-                    f"минут {self.dictionary_2[self.hour + 1]}")
+            self.modul_3()
         else:
             print("Что-то пошло не так!")
             exit()
@@ -147,14 +117,12 @@ class СonvertingTime:
     def start(self):
         self.logic_of_actions()
 
-
-# Without checking invalid values
 x = СonvertingTime()
-#x.start()
 try:
     x.start()
 except ValueError:
     print("Ошибка")
     print("_______"*4)
     print("Были введены некоректные данные")
+
 
