c898beddf88c4955e854e9b5c62a84e753f3d6b5
OxY623 <fuzzydoorproductions0@gmail.com>
2023-03-28 17:42:39 +0300

    Almost all

--- Tasks/Aleksiutovich/Task3/Task3_7.py
+++ Tasks/Aleksiutovich/Task3/Task3_7.py
@@ -1,17 +1,14 @@
-#Write a program that takes a string as input and returns the string with all capital letters converted to lowercase and vice versa.
-# user_input = input("Enter your text: \n")
-user_input = "Test is test. Uhe test a test"
+# Write a program that takes a string as input and returns the string with all capital letters converted to lowercase
+# and vice versa.
+user_input = input("Enter your text: \n")
 str_list = user_input.split()
-new_list = []
-print(str_list)
 for x in range(len(str_list)):
-    print(str_list[x])
     for y in range(len(str_list[x])):
-        print(str_list[x][y])
-
-        #Error
-        # Working
-
-
-
-
+        if str_list[x][y].islower():
+            str_list[x] = str_list[x].replace(str_list[x][y],
+                                              str_list[x][y].upper())
+        else:
+            str_list[x] = str_list[x].replace(str_list[x][y],
+                                              str_list[x][y].lower())
+output_str = ' '.join(str_list)
+print(output_str)
