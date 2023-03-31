d913b90d34ba5a40dd5e94895576df2a0d138a41
SvetlanaGanus <svetlanaganus@tut.by>
2023-03-31 17:48:01 +0300

    Update task3_4.py

--- Tasks/Ganus/task3_4.py
+++ Tasks/Ganus/task3_4.py
@@ -1,12 +1,17 @@
-
 str_list = input("Введите, пожалуйста, список чисел через пробел\n")
 str_list = str_list.split(" ")
-new_list = sorted(str_list)
+new_list = []
 number_count = -1
+for j in str_list:
+    j = int(j)
+    new_list.append(j)
+    new_list = sorted(new_list)
+   
 for i in range(len(new_list)):
     if new_list[number_count] != new_list[(number_count) - (i)]:
         print(new_list[(number_count) - (i)])
         break
+   
    
 
 
