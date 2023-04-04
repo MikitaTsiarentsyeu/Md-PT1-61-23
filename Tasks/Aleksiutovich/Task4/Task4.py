0842b7c8b761fc6244e106f6f57fa57761e69a0e
OxY623 <fuzzydoorproductions0@gmail.com>
2023-04-04 22:10:48 +0300

    Update Task4.py

--- Tasks/Aleksiutovich/Task4/Task4.py
+++ Tasks/Aleksiutovich/Task4/Task4.py
@@ -1,65 +1,32 @@
 # -*- coding: utf-8 -*-
 
+def format_text(file):
+    max_num_lines = int(input("Введите максимальное количество символов в строке  \n >"))
 
-def format_text(list_, max_chars):
-    """
+    if max_num_lines < 35:
+        print("Внимание!!!Принимается значение не менее 35!")
+        print("Повторите ввод!")
 
-    :param list:  одномерный список из слов
-    :param max_chars:  максимальное количество символов в строке
-    :return: отформатированный текст
-    """
-    num = len(list_)
-    lines = []
-    line = []
-    line_len = 0
-    for x in list_:
-        line.append('\t')
-        for word in x:
-            if line_len + len(word) + len(line) > max_chars:
-                for y in range(len(line)):
-                    line[y] += ' '
-                    if y < max_chars - line_len:
-                        line[y] += ''
-                lines.append(' '.join(line))
-                line = []
-                line_len = 0
-            line.append(word)
-            line_len += len(word)
-        line.append('\n')
-    lines.append(" ".join(line))
-    return '\n'.join(lines)
-
-
-def task_4(file):
-    """
-
-    :param file: Файл с текстом
-    :return: Записывает новый отфарм. файл
-    """
-    # max_num_lines = int(input("Enter maximum number of characters per line \n >"))
-    # if max_num_lines < 35:
-    #     print("Внимание!!!Принимается значение не менее 35!")
-    #     print("Повторите ввод")
-    #     task_4()
-    # else:
-
-    max_num_lines = 40
-    lines_ = []
-    with open(file) as f:
-        # cont = f.read()
-        # words = cont.split()
-        for line in f:
-            lines_.append(line)
-
-    # print(lines_)
-    for x in range(len(lines_)):
-        lines_[x] = lines_[x].split()
-    # print(lines_)
-    new_f = open('format_file.txt', 'w')
-    new_f.write(format_text(lines_, max_num_lines))
-    new_f.close()
-    print(format_text(lines_, max_num_lines))
-
+    else:
+# max_num_lines = 40
+        lists = []
+        with open(file) as f:
+            for line in f:
+                lists.append(line.split())
+        lines = []
+        current_line = ''
+        for list in lists:
+            for word in list:
+                if len(current_line) + len(word) + 1 <= max_num_lines:
+                    current_line += word + ' '
+                else:
+                    lines.append(current_line)
+                    current_line = word + ' '
+            lines.append(current_line)
+            current_line = ''
+        with open('format_file.txt', 'w') as new_f:
+            new_f.write('\n'.join(lines))
+            print("Файл создан и удачно записан!")
 
 text = 'text.txt'
-task_4(text)
+format_text(text)
