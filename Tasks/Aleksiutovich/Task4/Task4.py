ee099fc2c03a172a0bfb75b1e90ba17492785c5a
OxY623 <fuzzydoorproductions0@gmail.com>
2023-04-05 19:22:38 +0300

    Update Task4.py

--- Tasks/Aleksiutovich/Task4/Task4.py
+++ Tasks/Aleksiutovich/Task4/Task4.py
@@ -1,49 +1,96 @@
-# -*- coding: utf-8 -*-
-
+#-*- coding: utf-8 -*-
 def format_text(file):
+    """
+    This function opens a text file, formats it according
+    to the length of the line that it requests from the user.
+     And writes the formatted text to a new file.
+    Parameter:
+        file -- The str indicating the path to the file
+
+    """
+    # Запрос максимальной длины строки
     max_num_lines = int(input("""Enter the maximum number of characters per line.
 The number must be greater than 35.
 \\> """))
 
+    # Проверка правильности ввода максимальной длины
     if max_num_lines < 35:
         print("Attention. A value of at least 35 is accepted.")
         print("Error. Repeat the input.")
         raise ValueError("Invalid input! Number must be at least 35.")
 
+    # Если введенная максимальная длина верна, то приступаем к форматированию файла
     else:
-        # max_num_lines = 40
+        # Создаем список для хранения строк исходного файла
         lists = []
+        # Открываем файл и записываем его строки в созданный список
         with open(file) as f:
             for line in f:
                 lists.append(line.strip().split())
+        # Создаем список для хранения отформатированных строк
         lines = []
-        current_line = ''
+        # Создаем список для хранения слов текущей строки
+        current_line = []
+        # Обходим все строки исходного файла
         for list_ in lists:
+            # Обходим каждое слово в строке
             for word in list_:
-                if len(current_line) + len(word) + 1 <= max_num_lines:
-                    current_line += word + ' '
+                if len(' '.join(current_line)) + len(word) + 1 <= max_num_lines:
+                    # Если длина текущей строки плюс длина
+                    # следующего слова меньше максимальной длины строки,
+                    # то добавляем это слово в текущую строку
+                    current_line.append(word)
                 else:
-                    lines.append(current_line[:-1])  # .ljust(max_num_lines)
-                    current_line = word + ' '
-            lines.append(current_line[:-1])  # .ljust(max_num_lines)
-            current_line = ''
+                    # Если длина текущей строки плюс длина следующего
+                    # слова больше максимальной длины строки,
+                    # то записываем текущую строку
+                    # в список строк и начинаем новую строку
+                    lines.append(current_line)
+                    current_line = [word]
+            # Добавляем текущую строку в список строк
+            print(lines)
+            lines.append(current_line)
+            # Обнуляем текущую строку
+            current_line = []
+        # Создаем список для хранения окончательно отформатированных строк
         new_lines = []
+        # Обходим все строки
         for line in lines:
-            print(len(line))
-            if len(line) == max_num_lines:
-                new_lines.append(line.ljust(max_num_lines))
-            elif len(line) < max_num_lines:
-                y = max_num_lines - len(line)
-                line = line.replace(' ', '  ', y)
-                new_lines.append(line)
+            # Если длина строки равна максимальной длине,
+            # то сохраняем ее как есть
+            if len(' '.join(line)) == max_num_lines:
+                new_lines.append(' '.join(line))
+            # Если длина строки меньше максимальной длины,
+            # добавляем пробелы между словами,
+            # чтобы длина строки стала равна максимальной длине
+            elif len(' '.join(line)) < max_num_lines:
+                num_spaces = max_num_lines - len(''.join(line))
+                # space_counts количество пробелов
+                space_counts = [1] * len(line)
+                # Увеличиваем значение индексов,
+                # пока сумма space_counts не сравнится с num_spaces
+                while sum(space_counts) < num_spaces:
+                    for i in range(len(space_counts)):
+                        space_counts[i] += 1
+                        if sum(space_counts) == num_spaces:
+                            break
 
-        print(lines)
-        print(new_lines)
+                new_line = ''
+                # Обьединяем текущее слово с количеством пробелов,
+                # и добавляем все это в new_line.
+                for word, space_count in zip(line, space_counts):
+                    new_line += word + ' ' * space_count
+                # Удаляем все конечные пробелы и добавляем к списку
+                new_lines.append(new_line.rstrip())
 
+        # Создаем новый файл с отформатированными строками и закрываем его
         with open('format_file.txt', 'w') as new_f:
             new_f.write('\n'.join(new_lines))
-            print("The file has been created and successfully recorded.")
+        # Оповещаем  об этом пользователя
+        print("The file has been created and successfully recorded.")
 
 
-text = 'text.txt'
-format_text(text)
+
+file_now = 'text.txt'
+format_text(file_now)
+
