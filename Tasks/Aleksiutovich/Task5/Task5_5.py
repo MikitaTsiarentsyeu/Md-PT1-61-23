109060e58c72b2df78e2eacde7bf6970b7d768f3
OxY623 <fuzzydoorproductions0@gmail.com>
2023-04-09 19:03:48 +0300

    Update Task5_5.py

--- Tasks/Aleksiutovich/Task5/Task5_5.py
+++ Tasks/Aleksiutovich/Task5/Task5_5.py
@@ -4,7 +4,19 @@ import  random
 # get_ranges([0, 1, 2, 3, 4, 7, 8, 10])  ->  "0-4, 7-8, 10"
 # get_ranges([4,7,10])  -> "4, 7, 10"
 # get_ranges([2, 3, 8, 9])  -> "2-3, 8-9"
-
+def get_ranges(*numbers):
+    if not numbers:
+        return ""
+    ranges = []
+    start = end = numbers[0]
+    for num in numbers[1:]:
+        if num == end + 1:
+            end = num
+        else:
+            ranges.append(f"{start}-{end}" if start != end else f"{start}")
+            start = end = num
+    ranges.append(f"{start}-{end}" if start != end else f"{start}")
+    return ", ".join(ranges)
 #test
 def random_generator():
     list1 = [0, 1, 2, 3, 4, 7, 8, 10]
@@ -12,8 +24,6 @@ def random_generator():
     list3 = [2, 3, 8, 9]
     while True:
         yield random.choice(random.choice([list1, list2, list3]))
-
-num = random_generator()
-print(next(num))
-test = [next(num) if x%2==0 else x for x in range(random.randint(4,20))]
+test = [next(random_generator()) for x in range(random.randint(4,20))]
 print(test)
+print(get_ranges(test))
