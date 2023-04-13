6a6256da31835b00ee26c37dfba3f39a09ae195f
OxY623 <fuzzydoorproductions0@gmail.com>
2023-04-13 22:06:09 +0300

    Finish

--- Tasks/Aleksiutovich/Task5/Task_5_9.py
+++ Tasks/Aleksiutovich/Task5/Task_5_9.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
+from random import randint
+
+
 # Write a recursive function to calculate the power of a given number.
+def power(num, exponent):
+    if exponent == 0:
+        return 1
+    elif num == 0:
+        # иначе будет выводить 1
+        return 0
+    else:
+        return num * power(num, exponent - 1)
+
+
+# test
+# x = power(2,4)
+# x1 = power(1,3)
+# x2 = power(0,5)
+#
+# print(x)
+# print(x1)
+# print(x2)
+a, b = randint(0, 20), randint(0, 9)
+print(f'num = {a} and exponent = {b}')
+x = power(a, b)
+print(f'Result = {x}')
