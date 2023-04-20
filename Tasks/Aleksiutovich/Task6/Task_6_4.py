7aec2704a6d5a8be0b8e9546a6af6d341363e2fb
OxY623 <fuzzydoorproductions0@gmail.com>
2023-04-20 15:34:33 +0300

    Finish

--- Tasks/Aleksiutovich/Task6/Task_6_4.py
+++ Tasks/Aleksiutovich/Task6/Task_6_4.py
@@ -21,69 +21,65 @@ def log_to_file(file_name):
         return wrapper
     return log_decorator
 
-def logs_execution_time(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        start = time.time()
-        this_process = func(*args, **kwargs)
-        end = time.time()
-        time_work = start - end
-        logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
-                            format="%(asctime)s %(levelname)s %(message)s")
-        logging.info(f"The execution time is equal to {time_work}")
-        return this_process
-
-    return inner
-
-
-def logs_arguments(func):
-    @wraps(func)
-    def new_func(*args, **kwargs):
-        saved_args = locals()
-        logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
-                            format="%(asctime)s %(levelname)s %(message)s")
-        logging.info("The execution arguments : " + str(saved_args))
-        x = func(*args, **kwargs)
-        return x
-    return new_func
-
-
-def write_to_file(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        x = func(*args, **kwargs)
-        with open('test.txt', 'w') as f:
-            f.write(x)
-        return x
-    return inner
-
-
+# def logs_execution_time(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         this_process = func(*args, **kwargs)
+#         end = time.time()
+#         time_work = start - end
+#         logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
+#                             format="%(asctime)s %(levelname)s %(message)s")
+#         logging.info(f"The execution time is equal to {time_work}")
+#         return this_process
+#
+#     return wrapper
+#
+#
+# def logs_arguments(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         saved_args = locals()
+#         logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
+#                             format="%(asctime)s %(levelname)s %(message)s")
+#         logging.info("The execution arguments : " + str(saved_args))
+#         x = func(*args, **kwargs)
+#         return x
+#     return wrapper
+#
+#
+# def write_to_file(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         x = func(*args, **kwargs)
+#         with open('test.txt', 'w') as f:
+#             f.write(x)
+#         return x
+#     return wrapper
 
 
+
+# test
 # @logs_execution_time
 # @logs_arguments
 # @write_to_file
-@log_to_file('test1.txt')
-def function1(a, b):
-    c = a + b
+@log_to_file('Log_test1.txt')
+def function1(c, b):
+    c = c + b
     return f"This is the first function. A + B = {c}"
 
 
-@logs_execution_time
-@logs_arguments
-@write_to_file
+@log_to_file('Log_test2.txt')
 def function2(c):
     return "This is the second function."
 
 
-@logs_execution_time
-@logs_arguments
-@write_to_file
+@log_to_file('Log_test3.txt')
 def function3(a, b, c, d):
     return "This is the third function."
 
 
-# test
+
 print(function1(5, 6))
 print(function2("This is string"))
 print(function3(78, 545, [x for x in range(10)], {'y': 3, 'n': 6}))
